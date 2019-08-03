fa9dbc1bd99a074f456070053231ab46de80eb8f
wanara <34560418+wanara@users.noreply.github.com>
2019-08-04 07:26:02 +0900

    copy petitlyrics with copyright display limit
    add utaten copy lyrics logic

--- petitlyrics-catcher.js
+++ petitlyrics-catcher.js
@@ -1,50 +1,114 @@
 // ==UserScript==
 // @name         petitlyrics-catcher
 // @namespace    https://raw.githubusercontent.com/wanara/tampermonkey-script/master/petitlyrics-catcher.js
-// @version      0.2
+// @version      1.0
 // @description  a script to get lyrics in petitlyrics page, copy by a icon on the right side of "☆Bookmark this page"!
 // @author       wanara
 // @match        https://petitlyrics.com/lyrics/*
+// @match        https://utaten.com/lyric/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var lyrics_id = document.querySelector("meta[property='dable:item_id']").getAttribute('content');
-    //add a button to copy
-    var lists = document.querySelector("#social");
-    var li = document.createElement('li');
-    li.innerHTML = '<span class="ui-icon ui-icon-copy" id="clipboard" style="cursor: pointer;"></span>';
-    lists.appendChild(li);
-    var head= document.getElementsByTagName('head')[0];
-    var getLyric = () => {
-        console.log(Date.now());
-        //insert a script to get lyrics accroding to website
-        var script = document.createElement('script');
-        script.text = [
-            'var arr = [];',
-            '$.post(',
-            '  "/com/get_lyrics.ajax", {',
-            '    lyrics_id: ' + lyrics_id,
-            '  },',
-            '  function(data) {',
-            '      var len = data.length;',
-            '      for(var i=0; i<len; i++) arr.push(Base64.decode(data[i].lyrics));',
-            '      var clipboard = new ClipboardJS("#clipboard", {',
-            '          text: function() {',
-            '              return arr.join("\\n");',
-            '         }',
-            '      });',
-            '      clipboard.on("success", function(e) {',
-            '          console.log(e);',
-            '      });',
-            '      clipboard.on("error", function(e) {',
-            '          console.log(e);',
-            '      });',
-            '    }',
-            '  );'
-        ].join('\n');
-        head.appendChild(script);
+    const head= document.getElementsByTagName('head')[0];
+    const imgurl = "https://image.flaticon.com/icons/svg/157/157363.svg";
+    const width = "25";
+    const height = "25";
+    var getLyric = null;
+    //---
+    var img = document.createElement("img");
+    img.src = imgurl;
+    img.width = width;
+    img.height = height;
+    img.style = "cursor: pointer";
+    img.title = "Copy lyrics.";
+    if(window.location.hostname == "petitlyrics.com"){
+        const lyrics_meta = document.querySelector("meta[property='dable:item_id']");
+        var lyrics_id = lyrics_meta && lyrics_meta.getAttribute('content');
+        var lists = null;
+        if(lyrics_id == null){//check if lyrics can be displayed --- example:"https://petitlyrics.com/lyrics/2771167"
+            var pathclip = window.location.pathname.split("/");
+            if(pathclip.length > 2) lyrics_id = pathclip[2];
+            var bfrm1c = document.querySelector("div.bfrm1c div");
+            img.width = 20;
+            img.height = 20;
+            img.id = "clipboard";
+            bfrm1c.appendChild(img);
+        }else{
+            lists = document.querySelector("#social");
+            var li = document.createElement('li');
+            li.id = "clipboard";
+            li.appendChild(img);
+            lists.appendChild(li);//add a button to copy
+        }
+        if(!isNaN(lyrics_id) && lyrics_id > 0){
+            getLyric = () => {
+                console.log(Date.now());
+                //insert a script to get lyrics accroding to website
+                var script = document.createElement('script');
+                script.text = [
+                    'var arr = [];',
+                    '$.post(',
+                    '  "/com/get_lyrics.ajax", {',
+                    '    lyrics_id: ' + lyrics_id,
+                    '  },',
+                    '  function(data) {',
+                    '      var len = data.length;',
+                    '      for(var i=0; i<len; i++) arr.push(Base64.decode(data[i].lyrics));',
+                    '      var clipboard = new ClipboardJS("#clipboard", {',
+                    '          text: function() {',
+                    '              return arr.join("\\n");',
+                    '         }',
+                    '      });',
+                    '      clipboard.on("success", function(e) {',
+                    '          console.log(e);',
+                    '      });',
+                    '      clipboard.on("error", function(e) {',
+                    '          console.log(e);',
+                    '      });',
+                    '    }',
+                    '  );'
+                ].join('\n');
+                head.appendChild(script);
+            }
+        }
+    }
+    if(window.location.hostname == "utaten.com"){
+        const link = document.querySelector("ul.lyricLink");
+        var point = document.createElement("li");
+        point.id = "clipboard";
+        point.appendChild(img);
+        link.appendChild(point);
+        getLyric = () => {
+            console.log(Date.now());
+            //insert a script to convert lyrics
+            var script = document.createElement('script');
+            script.text = [
+                'var arr = [];',
+                'var line = "";',
+                'var child = document.querySelector(".hiragana").childNodes;',
+                'for(let i=0, len=child.length;i < len; i++){',
+                '   const nodename = child[i].nodeName;',
+                '   if(nodename == "BR"){arr.push(line); line = "";}',
+                '   else if(nodename == "SPAN"){line += child[i].querySelector(".rb").innerText}',
+                '   else if(line == ""){line = child[i].nodeValue.trimLeft()}',
+                '   else{line += child[i].nodeValue}',
+                '}',
+                'var clipboard = new ClipboardJS("#clipboard", {',
+                '   text: function() {',
+                '        return arr.join("\\n");',
+                '   }',
+                '});',
+                'clipboard.on("success", function(e) {',
+                '    console.log(e);',
+                '});',
+                'clipboard.on("error", function(e) {',
+                '    console.log(e);',
+                '});'
+            ].join('\n');
+            head.appendChild(script);
+        }
     }
     //require clipboard.js in the page
     var clipboard= document.createElement('script');
@@ -54,15 +118,5 @@
     console.log(Date.now());
     //execute getLyric after loading
     clipboard.addEventListener('load', getLyric);
-    
-    //utaten prepare
-//     var line = "";
-//     var child = document.querySelector(".hiragana").childNodes;
-//     for(let i=0, len=child.length;i < len; i++){
-//         const nodename = child[i].nodeName;
-//         if(nodename == "BR"){console.log(line); line = "";}
-//         else if(nodename == "SPAN"){line += child[i].querySelector(".rb").innerText}
-//         else if(line == ""){line = child[i].nodeValue.trimLeft()}
-//         else{line += child[i].nodeValue}
-//     }
+
 })();
